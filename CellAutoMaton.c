8b004019bba2c7046bdf8252243248a6059f2628
KentoTSUJI <s12t241@stmail.eng.kagawa-u.ac.jp>
2014-11-07 13:27:21 +0900

    dev: セルオートマトンの出力完了

--- CellAutoMaton.c
+++ CellAutoMaton.c
@@ -7,8 +7,8 @@
 // macro
 //============================================================
 #define RULE_LENGTH 8
-#define MAX_CHANGE 100
-#define MAX_SIZE 50
+#define MAX_CHANGE 50
+#define MAX_SIZE 100
 
 //============================================================
 // 大域変数
@@ -25,7 +25,7 @@ void output_tree(int cell_width, int p);
 void debug_arr(int length, int x[]);
 void apply_rule(int cell_width, int p);
 int value_to_cell(int cell_width, int level, int p);
-void get_cell();
+int get_cell(int cell_width, int level, int p);
 
 // main
 int main(int argc, char const* argv[])
@@ -33,16 +33,17 @@ int main(int argc, char const* argv[])
   // 変数群
   int change_num;     //状態変化回数
   int cell_width;     //セル幅
-  int p = 0;              //現在列
+  int p = 0;          //現在列
 
   // 変化回数の入力
   printf("input:change_num "); scanf("%d", &change_num);
   // ルール入力
   printf("input:rule "); input_rule();
   // 幅入力
-  printf("input:cell_width"); scanf("%d", &cell_width);
+  printf("input:cell_width "); scanf("%d", &cell_width);
   // seed入力
-  printf("input:seed"); input_seed(cell_width);
+  printf("input:seed "); input_seed(cell_width);
+  puts("");
   // 出力
   output_tree(cell_width, p);
   for ( p = 1; p < change_num; p++ ) {
@@ -61,7 +62,7 @@ void input_rule()
   for ( k = 0; k < RULE_LENGTH; k++ ) {
     scanf("%1d", &Rule[k]);
   }
-  // debug_arr(RULE_LENGTH, Rule);
+  debug_arr(RULE_LENGTH, Rule);
 }
 
 void input_seed(int cell_width)
@@ -74,23 +75,55 @@ void input_seed(int cell_width)
 void output_tree(int cell_width, int p)
 {
   int k;
+  printf("%3d : ", p);
   for ( k = 0; k < cell_width; k++ ) {
-    printf("%d", Cell[p][k]);
+    switch ( Cell[p][k] ) {
+      case 0:
+        printf(" ");
+        break;
+      case 1:
+        printf("*");
+        break;
+    }
+    // printf("%d", Cell[p][k]);
   }
+  puts("");
 }
 
 void apply_rule(int cell_width, int p)
 {
   int k;
   for ( k = 0; k < cell_width; k++ ) {
-    Cell[p][k] = value_to_cell(cell_width, p, k);
+    Cell[p][k] = value_to_cell(cell_width, p-1, k);
   }
 }
 
 int value_to_cell(int cell_width, int level, int p)
 {
+
+  int left;
+  int center;
+  int right;
+  int rule_value;
+
+  left   = get_cell(cell_width, level, p-1);
+  center = get_cell(cell_width, level, p);
+  right  = get_cell(cell_width, level, p+1);
+
+  rule_value = left * 4 + center * 2 + right * 1;
+
+  return Rule[rule_value];
 }
-void get_cell();
+
+int get_cell(int cell_width, int level, int p)
+{
+  // 範囲外判定
+  if ( p < 0  ) { p = cell_width-1; }
+  if ( p >= 0 ) { p = 0; }
+
+  return Cell[level][p];
+}
+
 void debug_arr(int length, int x[])
 {
   int k;
