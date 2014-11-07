f90c88d0b84ae62142c193f5b49eaabafdc59397
KentoTSUJI <s12t241@stmail.eng.kagawa-u.ac.jp>
2014-11-07 12:01:39 +0900

    dev: Rule入力関数の実装

--- CellAutoMaton.c
+++ CellAutoMaton.c
@@ -22,6 +22,7 @@ int Cell[MAX_CHANGE][MAX_SIZE];
 void input_rule();
 void input_seed(int cell_width);
 void output_tree(int cell_width, int change_num);
+void debug_arr(int length, int x[]);
 
 // main
 int main(int argc, char const* argv[])
@@ -31,15 +32,33 @@ int main(int argc, char const* argv[])
   int cell_width;     //セル幅
 
   // 変化回数の入力
-  scanf("%d", &change_num);
+  printf("input:change_num "); scanf("%d", &change_num);
   // ルール入力
-  input_rule();
+  printf("input:rule "); input_rule();
   // 幅入力
-  scanf("%d", &cell_width);
+  // printf("input:change_num"); scanf("%d", &cell_width);
   // seed入力
-  input_seed(cell_width);
+  // iprintf("input:change_num"); nput_seed(cell_width);
   // 出力
-  output_tree(cell_width, change_num);
+  // output_tree(cell_width, change_num);
 
   return 0;
+}
+
+void input_rule()
+{
+  int k;
+  for ( k = 0; k < RULE_LENGTH; k++ ) {
+    scanf("%1d", &Rule[k]);
+  }
+  // debug_arr(RULE_LENGTH, Rule);
+}
+
+void debug_arr(int length, int x[])
+{
+  int k;
+  for ( k = 0; k < RULE_LENGTH; k++ ) {
+    printf("%d", x[k]);
+  }
+  puts("");
 }
